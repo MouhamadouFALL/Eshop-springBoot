9c5f191c8cc0818033a50deb95ed0fdbca4d28d7
Mouhamadou FALL <mouhamadou2.fall@gmail.com>
2022-07-20 16:52:11 +0000

    initialize projet Eshop

---
+++ siggishopWebParent/siggishopBackEnd/src/main/java/sn/siggishop/admin/SiggishopBackEndApplication.java
@@ -0,0 +1,13 @@
+package sn.siggishop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiggishopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiggishopBackEndApplication.class, args);
+	}
+
+}
---
+++ siggishopWebParent/siggishopFrontEnd/src/main/java/sn/siggishop/site/SiggishopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package sn.siggishop.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiggishopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiggishopFrontEndApplication.class, args);
+	}
+
+}
